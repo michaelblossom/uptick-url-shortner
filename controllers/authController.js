feb7ae05dbb307b520696a34c9a80f23eec0948b
michael <michaelblossom8654@gmail.com>
2024-03-31 11:53:41 +0530

    added authentication features to the application

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,9 +2,9 @@ const crypto = require("crypto"); //this is a built in function basically use fo
 // requiring built in util for writing async function(verification of token)
 const { promisify } = require("util"); //builtin function for promifying token verification
 const jwt = require("jsonwebtoken");
-const User = require("../models/userModel");
-const catchAsync = require("../utils/catchAsync");
-const AppError = require("../utils/appError");
+const User = require("./../models/userModel");
+const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
 
 // function to generate token
 const signToken = (id) => {
@@ -73,47 +73,47 @@ exports.login = catchAsync(async (req, res, next) => {
 });
 
 // PROTECT MIDDLEWARE
-// exports.protect = catchAsync(async (req, res, next) => {
-//   // 1)Getting token and check if it exist
-//   let token;
-//   if (
-//     req.headers.authorization &&
-//     req.headers.authorization.startsWith("Bearer")
-//   ) {
-//     token = req.headers.authorization.split(" ")[1];
-//   } else if (req.cookies.jwt) {
-//     token = req.cookie.jwt;
-//   }
-//   if (!token) {
-//     // console.log(token);
-//     return next(
-//       new AppError("you are not logged please login to get access", 401)
-//     );
-// }
-// // 2)verify the token
-// const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-// // console.log(decoded);
-// // 3)check if the user accessing the route still exist
-// const currentUser = await User.findById(decoded.id); //we are using findById because we use our Id as our payload in generating the token that is stored in our decoded
-// if (!currentUser) {
-//   return next(
-//     new AppError(
-//       "The User belonging to this token does not exist anylonger",
-//       401
-//     )
-//     );
-//   }
-//   // 4)check if user change password after token was issued
-//   // calling passwordchangedAfter function from userModel
-//   if (currentUser.changedPasswordAfter(decoded.iat)) {
-//     return next(
-//       new AppError("User recently changed password please login again", 401)
-//     );
-//   }
-//   // GRANT ACCESS TO PROTECTED ROUTE
-//   req.user = currentUser;
-//   next();
-// });
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1)Getting token and check if it exist
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookie.jwt;
+  }
+  if (!token) {
+    // console.log(token);
+    return next(
+      new AppError("you are not logged please login to get access", 401)
+    );
+  }
+  // 2)verify the token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  // console.log(decoded);
+  // 3)check if the user accessing the route still exist
+  const currentUser = await User.findById(decoded.id); //we are using findById because we use our Id as our payload in generating the token that is stored in our decoded
+  if (!currentUser) {
+    return next(
+      new AppError(
+        "The User belonging to this token does not exist anylonger",
+        401
+      )
+    );
+  }
+  // 4)check if user change password after token was issued
+  // calling passwordchangedAfter function from userModel
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError("User recently changed password please login again", 401)
+    );
+  }
+  // GRANT ACCESS TO PROTECTED ROUTE
+  req.user = currentUser;
+  next();
+});
 
 //  restrictions
 exports.restrictTo = (...roles) => {
